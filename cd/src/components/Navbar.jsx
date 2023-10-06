f7eedf5f4952f1213377033934e2ffa80d61c322
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-10-06 17:19:09 +0500

    navbar finished

--- cd/src/components/Navbar.jsx
+++ cd/src/components/Navbar.jsx
@@ -1,18 +1,29 @@
 import { BsFillCartCheckFill } from "react-icons/bs";
 import { NavLink, Link } from "react-router-dom";
 import Logo from "../../public/Logo.svg";
+import { useState } from "react";
 import React from "react";
-import './Navbar.scss'
+import "./Navbar.scss";
 const Navbar = () => {
+  const [togle, setTogle] = useState(false);
+  const togleFunction = () => {
+    return setTogle(!togle);
+  };
   return (
     <div className='navbar bg-light'>
       <div className='container'>
         <div className='nav d-flex w-100 align-items-center justify-content-between'>
           <img src={Logo} alt='Logo' />
-          <div className='nav__list d-flex d-none align-item-center justify-content-center'>
-            <NavLink className='text-decoration-none nav__list--item d-block'>Shop</NavLink>
-            <NavLink className='text-decoration-none nav__list--item d-block'>About Us</NavLink>
-            <NavLink className='text-decoration-none nav__list--item d-block'>Our team</NavLink>
+          <div className='nav__list d-lg-flex d-none align-item-center justify-content-center'>
+            <NavLink className='text-decoration-none nav__list--item d-block'>
+              Shop
+            </NavLink>
+            <NavLink className='text-decoration-none nav__list--item d-block'>
+              About Us
+            </NavLink>
+            <NavLink className='text-decoration-none nav__list--item d-block'>
+              Our team
+            </NavLink>
             <NavLink className='text-decoration-none nav__list--item d-block'>
               Buyer's guides
             </NavLink>
@@ -20,15 +31,58 @@ const Navbar = () => {
               Categories
             </NavLink>
           </div>
-          <div className='nav__link d-flex align-items-center '>
-            <Link className='d-block nav__link--item text-decoration-none'>Buy template</Link>
+          {togle ? (
+            <div className=' position-absolute d-flex justify-content-center nav__bg   top-100'>
+              <div className=''>
+                <div className='pt-5 '>
+                  <NavLink className='text-decoration-none pb-4 text-center nav__list--item d-block'>
+                    Shop
+                  </NavLink>
+                  <NavLink className='text-decoration-none pb-4 text-center nav__list--item d-block'>
+                    About Us
+                  </NavLink>
+                  <NavLink className='text-decoration-none pb-4 text-center nav__list--item d-block'>
+                    Our team
+                  </NavLink>
+                  <NavLink className='text-decoration-none pb-4 text-center nav__list--item d-block'>
+                    Buyer's guides
+                  </NavLink>
+                  <NavLink className='text-decoration-none pb-4 text-center nav__list--item d-block'>
+                    Categories
+                  </NavLink>
+                </div>
+                <div className='nav__link  d-flex align-items-center '>
+                  <Link className='d-block nav__link--item text-decoration-none'>
+                    Buy template
+                  </Link>
+                  <Link className='d-block nav__link--item d-flex align-items-center text-decoration-none'>
+                    <BsFillCartCheckFill className='d-block pe-1 fs-4' />
+                    Cart
+                    <span className='nav__link--zero'>0</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className='nav__link d-none d-lg-flex align-items-center '>
+            <Link className='d-block nav__link--item text-decoration-none'>
+              Buy template
+            </Link>
             <Link className='d-block nav__link--item d-flex align-items-center text-decoration-none'>
-             
-              <BsFillCartCheckFill className='d-block pe-1 fs-4'  />
+              <BsFillCartCheckFill className='d-block pe-1 fs-4' />
               Cart
-              <span className="nav__link--zero">0</span>
+              <span className='nav__link--zero'>0</span>
             </Link>
           </div>
+          <button
+            onClick={togleFunction}
+            className='border-0 d-lg-none bg-transparent'>
+            <span className='d-block   nav__leni bg-dark'></span>
+            <span className='d-block  nav__leni bg-dark'></span>
+            <span className='d-block  nav__leni bg-dark'></span>
+          </button>
         </div>
       </div>
     </div>
