581a7dad8396d9bf683788ef0c4b53f28c59f7c6
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-10-06 16:23:48 +0500

    I made an update

--- cd/src/components/Navbar.jsx
+++ cd/src/components/Navbar.jsx
@@ -1,30 +1,31 @@
-import React from "react";
-import Logo from "../../public/Logo.svg";
-import { NavLink, Link } from "react-router-dom";
 import { BsFillCartCheckFill } from "react-icons/bs";
+import { NavLink, Link } from "react-router-dom";
+import Logo from "../../public/Logo.svg";
+import React from "react";
+import './Navbar.scss'
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className='navbar bg-light'>
       <div className='container'>
-        <div className='nav'>
+        <div className='nav d-flex w-100 align-items-center justify-content-between'>
           <img src={Logo} alt='Logo' />
-          <div className='nav__list'>
-            <NavLink className='text-decoration-none d-block'>Shop</NavLink>
-            <NavLink className='text-decoration-none d-block'>About Us</NavLink>
-            <NavLink className='text-decoration-none d-block'>Our team</NavLink>
-            <NavLink className='text-decoration-none d-block'>
+          <div className='nav__list d-flex d-none align-item-center justify-content-center'>
+            <NavLink className='text-decoration-none nav__list--item d-block'>Shop</NavLink>
+            <NavLink className='text-decoration-none nav__list--item d-block'>About Us</NavLink>
+            <NavLink className='text-decoration-none nav__list--item d-block'>Our team</NavLink>
+            <NavLink className='text-decoration-none nav__list--item d-block'>
               Buyer's guides
             </NavLink>
-            <NavLink className='text-decoration-none d-block'>
+            <NavLink className='text-decoration-none nav__list--item d-block'>
               Categories
             </NavLink>
           </div>
-          <div className='nav__link'>
-            <Link className='d-block text-decoration-none'>Buy template</Link>
-            <Link className='d-block text-decoration-none'>
+          <div className='nav__link d-flex align-items-center '>
+            <Link className='d-block nav__link--item text-decoration-none'>Buy template</Link>
+            <Link className='d-block nav__link--item d-flex align-items-center text-decoration-none'>
              
-              <BsFillCartCheckFill />
-              cart
+              <BsFillCartCheckFill className='d-block pe-1 fs-4'  />
+              Cart
               <span className="nav__link--zero">0</span>
             </Link>
           </div>
